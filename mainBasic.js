e03308ae446a282479505bbb36572aa81c84430c
john sander <john sander>
2017-05-05 22:45:21 -0400

    basic updated

---
+++ mainBasic.js
@@ -0,0 +1,7 @@
+var BasicCard = require("./BasicCard.js");
+
+var mascot = new BasicCard(
+    "Who is the Charlotte Hornet's mascot?", "Hugo");
+
+console.log("Front: " + mascot.front);
+console.log("Back: " + mascot.back);
